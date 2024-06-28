74b3cdc96b8151dc65bb9a1a244e57b3532411d9
Adham Khatean <aekhatean@gmail.com>
2024-06-29 01:40:33 +0300

    seed data on db initialization

--- init.sql
+++ init.sql
@@ -60,3 +60,53 @@ CREATE TABLE friendships (
     CONSTRAINT fk_person_2_id FOREIGN KEY (person_2)
         REFERENCES users(id)
 );
+
+
+-- Data seeding
+INSERT INTO users (first_name, last_name, email, birthdate, password)
+VALUES
+    ('John', 'Doe', 'john.doe@example.com', '1990-05-15', 'password123'),
+    ('Jane', 'Smith', 'jane.smith@example.com', '1992-08-20', 'securepwd'),
+    ('Michael', 'Johnson', 'michael.johnson@example.com', '1985-12-10', 'strongpass'),
+    ('Emily', 'Brown', 'emily.brown@example.com', '1994-03-25', 'pass123word'),
+    ('David', 'Williams', 'david.williams@example.com', '1988-06-08', 'safePassword');
+
+INSERT INTO posts (author_id, content)
+VALUES
+    (1, 'This is the first post by John Doe.'),
+    (2, 'Jane Smith shares her thoughts.'),
+    (3, 'Michael Johnson updates on his project.'),
+    (4, 'Emily Brown posts a photo.'),
+    (5, 'David Williams shares an article.');
+
+INSERT INTO comments (author_id, post_id, content)
+VALUES
+    (2, 1, 'Interesting!'),
+    (3, 1, 'Great post.'),
+    (4, 3, 'Looking forward to it.'),
+    (5, 2, 'Nice thoughts.'),
+    (1, 4, 'Beautiful photo!');
+
+INSERT INTO likes (reactor_id, post_id)
+VALUES
+    (3, 1),
+    (5, 1),
+    (2, 3),
+    (4, 2),
+    (1, 5);
+
+INSERT INTO shares (sharer_id, post_id)
+VALUES
+    (4, 1),
+    (1, 2),
+    (5, 3),
+    (2, 4),
+    (3, 5);
+
+INSERT INTO friendships (person_1, person_2)
+VALUES
+    (1, 2),
+    (3, 4),
+    (5, 1),
+    (2, 3),
+    (4, 5);
