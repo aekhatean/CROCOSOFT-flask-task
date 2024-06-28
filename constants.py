eb3d40fdf2e1151ea5deb4547269e31c0d95a8c3
Adham Khatean <aekhatean@gmail.com>
2024-06-29 01:41:03 +0300

    use env variables in python code

---
+++ constants.py
@@ -0,0 +1,6 @@
+from os import environ
+
+MYSQL_DATABASE = environ.get('MYSQL_DATABASE')
+MYSQL_USER = environ.get('MYSQL_USER')
+MYSQL_PASSWORD = environ.get('MYSQL_PASSWORD')
+MYSQL_ROOT_PASSWORD = environ.get('MYSQL_ROOT_PASSWORD')
