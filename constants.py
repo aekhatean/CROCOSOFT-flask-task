f30fdce0115b9350a69c8fc284860389b515b4d2
Adham Khatean <aekhatean@gmail.com>
2024-06-29 03:01:40 +0300

    ammend contants.py for DB_CONFIG quick access

--- constants.py
+++ constants.py
@@ -4,3 +4,9 @@ MYSQL_DATABASE = environ.get('MYSQL_DATABASE')
 MYSQL_USER = environ.get('MYSQL_USER')
 MYSQL_PASSWORD = environ.get('MYSQL_PASSWORD')
 MYSQL_ROOT_PASSWORD = environ.get('MYSQL_ROOT_PASSWORD')
+DB_CONF = {
+    "host": "db",  # Use the service name defined in docker-compose.yml
+    "database": MYSQL_DATABASE,
+    "user": MYSQL_USER,
+    "password": MYSQL_PASSWORD,
+}
